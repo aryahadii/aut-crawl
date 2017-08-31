4738dea49411b597f6708e89fc3d449c62ec7d50
Arya Hadi <arya@cafebazaar.ir>
2017-08-31 17:52:43 +0430

    Add crawler

--- crawler/main.py
+++ crawler/main.py
@@ -1,7 +1,11 @@
 import sys
 import getopt
 
-DEFAULT_COURSES_URL = "https://education.aut.ac.ir/aportal/regadm/global/popup.sem.courses.jsp?action=view&course_id=st_sem_course&cs_semester={}{}&cur_sort_col=CourseId&cur_sort_ord=ASC&cur_page={}"
+import course_crawler
+
+DEFAULT_COURSES_URL = 'https://education.aut.ac.ir/aportal/regadm/global/' \
+    'popup.sem.courses.jsp?action=view&course_id=st_sem_course&cs_semester=' \
+    '{}{}&cur_sort_col=CourseId&cur_sort_ord=ASC&cur_page={}'
 
 
 def main(argv=None):
@@ -11,11 +15,21 @@ def main(argv=None):
         opts, args = getopt.getopt(
             argv[1:], "uys", ["url", "year", "semester"])
     except getopt.GetoptError as err:
-        print(err)
+        print err
         sys.exit(2)
 
+    url = DEFAULT_COURSES_URL
+    year = '96'
+    semester = '1'
     for opt, arg in opts:
-        pass
+        if opt == '-u':
+            url = arg
+        elif opt == '-y':
+            year = arg
+        elif opt == '-s':
+            semester = arg
+
+    course_crawler.crawl_courses(url, year, semester)
 
 
 if __name__ == '__main__':
