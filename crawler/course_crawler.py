ddff0ec74dd83200c7b102ab113cca1a875a642b
Arya Hadi <arya@cafebazaar.ir>
2017-08-31 15:33:17 +0430

    Add opts

---
+++ crawler/course_crawler.py
@@ -0,0 +1,3 @@
+def crawl_courses(url=None, out_file_path='courses.out'):
+    if url is None:
+        return
--- crawler/main.py
+++ crawler/main.py
@@ -1,11 +1,21 @@
 import sys
 import getopt
 
+DEFAULT_COURSES_URL = "https://education.aut.ac.ir/aportal/regadm/global/popup.sem.courses.jsp?action=view&course_id=st_sem_course&cs_semester={}{}&cur_sort_col=CourseId&cur_sort_ord=ASC&cur_page={}"
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    opts, args = getopt.getopt(argv[1:], "u", ["url"])
+    try:
+        opts, args = getopt.getopt(
+            argv[1:], "uys", ["url", "year", "semester"])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        pass
 
 
 if __name__ == '__main__':
